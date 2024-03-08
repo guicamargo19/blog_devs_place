f91914df3a11ef094c03a077ec8cd5ea5c01a335
camargo <gui.ferreira.camargo@gmail.com>
2024-03-08 19:57:50 -0300

    Adicionado model Post e relação com Category e Tags

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,7 +1,7 @@
 # type: ignore
 from django.contrib import admin
 
-from blog.models import Tag, Category, Page
+from blog.models import Tag, Category, Page, Post
 
 
 @admin.register(Tag)
@@ -40,3 +40,19 @@ class PageAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         'slug': ('title',),
     }
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'is_published', 'created_by',
+    list_display_links = 'title',
+    search_fields = 'id', 'slug', 'title', 'excerpt', 'content',
+    list_per_page = 50
+    list_filter = 'category', 'is_published',
+    list_editable = 'is_published',
+    ordering = '-id',
+    readonly_fields = 'created_at', 'updated_at', 'updated_by', 'created_by',
+    prepopulated_fields = {
+        'slug': ('title',),
+    }
+    autocomplete_fields = 'tags', 'category',
