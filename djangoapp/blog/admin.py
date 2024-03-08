0c5b6f2887eb13cef640a53ca8431ff1193357c0
camargo <gui.ferreira.camargo@gmail.com>
2024-03-08 19:00:29 -0300

    Criado o model de Pages e Categories

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,7 +1,7 @@
 # type: ignore
 from django.contrib import admin
 
-from blog.models import Tag
+from blog.models import Tag, Category, Page
 
 
 @admin.register(Tag)
@@ -13,4 +13,30 @@ class TagAdmin(admin.ModelAdmin):
     ordering = '-id',
     prepopulated_fields = {
         'slug': ('name',),
+    }
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'slug',
+    list_display_links = 'name',
+    search_fields = 'id', 'name', 'slug',
+    list_per_page = 10
+    ordering = '-id',
+    prepopulated_fields = {
+        'slug': ('name',),
+    }
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'slug', 'is_published',
+    list_display_links = 'title',
+    search_fields = 'id', 'title', 'slug', 'content',
+    list_per_page = 50
+    list_filter = 'is_published',
+    list_editable = 'is_published',
+    ordering = '-id',
+    prepopulated_fields = {
+        'slug': ('title',),
     }
--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -21,3 +21,56 @@ class Tag(models.Model):
         if not self.slug:
             self.slug = new_slugify(self.name, 5)
         return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        unique=True,
+        default=None,
+        blank=True,
+        max_length=255,
+        null=True
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = new_slugify(self.name, 5)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True,
+        default="",
+        blank=True,
+        max_length=255,
+        null=False
+    )
+
+    is_published = models.BooleanField(
+        default=False,
+        help_text=('Este campo precisa estar marcado para que '
+                   'a página seja exibida publicamente.'
+                   )
+    )
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = new_slugify(self.title, 5)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.title
