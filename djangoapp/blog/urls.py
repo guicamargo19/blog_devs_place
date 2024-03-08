8a1d5544fb68afba3de435e8fd95abf83fabdd0a
camargo <gui.ferreira.camargo@gmail.com>
2024-03-08 15:42:48 -0300

    Criado arquivo de estilos, templates e views

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,10 +1,12 @@
 # type: ignore
 
 from django.urls import path
-from blog.views import index
+from blog.views import index, post, page
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('post/', post, name='post'),
+    path('page/', page, name='page'),
 ]
