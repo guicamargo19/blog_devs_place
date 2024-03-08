19a88f7fad48df9cf7f304b73b5013f84ecb5e19
camargo <gui.ferreira.camargo@gmail.com>
2024-03-08 11:58:54 -0300

    Adicionado context_processor

---
+++ djangoapp/site_setup/context_processor.py
@@ -0,0 +1,9 @@
+# type: ignore
+from site_setup.models import SiteSetup
+
+
+def site_setup(request):
+    setup = SiteSetup.objects.order_by('-id').first()
+    return {
+        'site_setup': setup,
+    }
