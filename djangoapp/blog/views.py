192cda4db7319c54506f9cbf130c3ed657a483b2
camargo <gui.ferreira.camargo@gmail.com>
2024-03-09 12:24:11 -0300

    Adicionado posts reais no blog

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,12 +1,15 @@
 # type: ignore
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from blog.models import Post
 
-posts = list(range(1000))
+PER_PAGE = 9
 
 
 def index(request):
-    paginator = Paginator(posts, 9)
+    posts = Post.objects.filter(is_published=True).order_by('-pk')
+
+    paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
@@ -20,28 +23,16 @@ def index(request):
 
 
 def page(request):
-    paginator = Paginator(posts, 9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
 
     return render(
         request,
         'blog/pages/page.html',
-        {
-            'page_obj': page_obj,
-        }
     )
 
 
 def post(request):
-    paginator = Paginator(posts, 9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
 
     return render(
         request,
         'blog/pages/post.html',
-        {
-            'page_obj': page_obj,
-        }
     )
