07ef82bb4bea796d9e1222f3427009a7dd2565f3
camargo <gui.ferreira.camargo@gmail.com>
2024-03-13 11:09:10 -0300

    Adicionado títulos nas páginas e configurado campo search

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,7 +1,7 @@
 # type: ignore
 
 from django.urls import path
-from blog.views import index, post, page, created_by, category, tag
+from blog.views import index, post, page, created_by, category, tag, search
 
 app_name = 'blog'
 
@@ -12,4 +12,5 @@ urlpatterns = [
     path('created_by/<int:author_pk>/', created_by, name='created_by'),
     path('category/<slug:slug>/', category, name='category'),
     path('tag/<slug:slug>/', tag, name='tag'),
+    path('search/', search, name='search'),
 ]
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,7 +1,11 @@
 # type: ignore
 from django.shortcuts import render
 from django.core.paginator import Paginator
-from blog.models import Post
+from blog.models import Post, Page
+from django.db.models import Q
+from django.http import Http404
+from django.contrib.auth.models import User
+
 
 PER_PAGE = 9
 
@@ -18,31 +22,65 @@ def index(request):
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': 'Home - ',
         }
     )
 
 
 def page(request, slug):
+    page_obj = (
+        Page.objects
+        .filter(is_published=True)
+        .filter(slug=slug)
+        .first()
+    )
+
+    if page_obj is None:
+        raise Http404()
+
+    page_title = f'{page_obj.title} - Página - '
 
     return render(
         request,
         'blog/pages/page.html',
+        {
+            'page': page_obj,
+            'page_title': page_title,
+        }
+
     )
 
 
 def post(request, slug):
-    post = Post.objects.get_published().filter(slug=slug).first()
+    post_obj = Post.objects.get_published().filter(slug=slug).first()
+
+    if post_obj is None:
+        raise Http404()
+
+    page_title = f'{post_obj.title} - Post - '
 
     return render(
         request,
         'blog/pages/post.html',
         {
-            'post': post,
+            'post': post_obj,
+            'page_title': page_title,
         }
     )
 
 
 def created_by(request, author_pk):
+    user = User.objects.filter(pk=author_pk).first()
+
+    if user is None:
+        raise Http404()
+
+    user_full_name = user.username
+
+    if user.first_name:
+        user_full_name = f'{user.first_name} {user.last_name}'
+    page_title = user_full_name + ' posts - '
+
     posts = Post.objects.get_published().filter(created_by__pk=author_pk)
 
     paginator = Paginator(posts, PER_PAGE)
@@ -54,6 +92,7 @@ def created_by(request, author_pk):
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': page_title,
         }
     )
 
@@ -65,11 +104,17 @@ def category(request, slug):
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
+    if len(page_obj) == 0:
+        raise Http404()
+
+    page_title = f'{page_obj[0].category.name} - Categoria - '
+
     return render(
         request,
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': page_title,
         }
     )
 
@@ -81,10 +126,40 @@ def tag(request, slug):
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
+    if len(page_obj) == 0:
+        raise Http404()
+
+    page_title = f'{page_obj[0].tags.first().name} - Tag - '
+
     return render(
         request,
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': page_title,
+        }
+    )
+
+
+def search(request):
+    search_value = request.GET.get('search', '').strip()
+    posts = (
+        Post.objects.get_published()
+        .filter(
+            Q(title__icontains=search_value) |
+            Q(excerpt__icontains=search_value) |
+            Q(content__icontains=search_value)
+        )[:PER_PAGE]
+    )
+
+    page_title = f'{search_value[:30]} - Search - '
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': posts,
+            'search_value': search_value,
+            'page_title': page_title,
         }
     )
